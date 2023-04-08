4dfdbd93f031fcade6c165415d092c327555b693
ꜱᴛᴀʀᴋ <120701871+NotStark@users.noreply.github.com>
2023-04-08 11:56:21 +0530

    Update example.py

--- example.py
+++ example.py
@@ -4,10 +4,10 @@ from Peakpx import PeakPx
 px = PeakPx()
 
 #get wallpapers
-wallpapers = px.get_wallpaers() #it will return a list of wallpapers
+wallpapers = px.get_wallpapers() #it will return a list of wallpapers
 
 #pagination 
-wallpapers = px.get_wallpaers(page=2) #by deafult its one
+wallpapers = px.get_wallpapers(page=2) #by deafult its one
 
 #search wallpapers
 wallpapers = px.search_wallpapers(query='one piece') #you can also paas a page parameter here like page =2 by default its 1
